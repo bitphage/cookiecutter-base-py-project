5af8d5904f4065a8781ca60759a4483c1e70a221
Vladimir Kamarzin <vvk@vvk.pp.ru>
2020-04-02 15:29:37 +0500

    Switch to pipenv

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
 
 git init
-virtualenv -p python3.7 --prompt="({{ cookiecutter.project_name }}) " {{ cookiecutter.virtualenv }}
-{{ cookiecutter.virtualenv }}/bin/python -m pip install -U pip
-{{ cookiecutter.virtualenv }}/bin/python -m pip install -r requirements.txt
-{{ cookiecutter.virtualenv }}/bin/python -m pre_commit install
-{{ cookiecutter.virtualenv }}/bin/python setup.py develop
+pipenv install --dev pre-commit black isort flake8
