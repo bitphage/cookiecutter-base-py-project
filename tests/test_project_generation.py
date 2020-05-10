8e3c093557b043bec5191205a5cc2885ae2985a4
Vladimir Kamarzin <vvk@vvk.pp.ru>
2020-05-10 11:59:52 +0500

    Add test to test project generation

---
+++ tests/test_project_generation.py
@@ -0,0 +1,7 @@
+def test_bake_project(cookies):
+    result = cookies.bake()
+
+    assert result.exit_code == 0
+    assert result.exception is None
+    assert result.project.basename == 'test-project'
+    assert result.project.isdir()
